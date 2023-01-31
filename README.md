1f6952a9f70059b5005cc62feb8f5245c1e70678
Manueldavid77 <81543336+Manueldavid77@users.noreply.github.com>
2023-01-31 15:57:26 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EnQueMeEstoyMetiendo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-pqpcrn)
---
+++ index.js
@@ -0,0 +1,10 @@
+for (let factorial = 0; factorial < 10; factorial++) {
+  console.log(factorial)
+}
+
+let factorial = 10;
+while (factorial < 11) {
+  factorial++;
+  
+}
+
