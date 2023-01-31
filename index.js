dea64e20b27afbc306d063ef54caeeea5f7fd8cd
Manueldavid77 <81543336+Manueldavid77@users.noreply.github.com>
2023-01-31 16:02:03 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-for (let factorial = 0; factorial < 10; factorial++) {
-  console.log(factorial)
+var factorial = 1
+for (let i = 10; i > 0; i--) {
+    factorial *= i
 }
+console.log(factorial)
 
-let factorial = 10;
-while (factorial < 11) {
-  factorial++;
-  
+var factorial = 1
+var num = 10
+while (num > 1) {
+    factorial *= num
+    num--
 }
+console.log(factorial)
 
+
+var factorial = 1
+var num = 10
+while (true) {
+    factorial *= num
+    num--
+    if (num === 1) break
+}
+console.log(factorial)
